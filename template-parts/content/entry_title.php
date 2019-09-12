e3861d00f05a4429d3a7fd7513e79bf17d3d1b48
Alex Vasquez <alexjvas@gmail.com>
2019-09-11 23:12:28 -0700

    Added acf field to hide title.

--- template-parts/content/entry_title.php
+++ template-parts/content/entry_title.php
@@ -7,8 +7,12 @@
 
 namespace WP_Rig\WP_Rig;
 
+$hide_title = get_field( 'dgs_remove_title' );
+
 if ( is_singular( get_post_type() ) ) {
-	the_title( '<h1 class="entry-title">', '</h1>' );
+	if ( empty( $hide_title ) ) {
+		the_title( '<h1 class="entry-title">', '</h1>' );
+	}
 } else {
 	the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 }
